88ccf13bc85321318da47732b9d0e2c3bd02ed57
Dylan Moss <52764361+DylanTMoss@users.noreply.github.com>
2021-03-25 09:59:57 -0500

    add cell cost handling

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,11 +6,28 @@ public class Cell
 {
     public Vector3 worldPos;
     public Vector2Int gridIndex;
+    public byte cost;
 
-    
     public Cell(Vector3 worldPos, Vector2Int gridIndex)
     {
         this.worldPos = worldPos;
         this.gridIndex = gridIndex;
+        cost = 1;
+    }
+
+    public void IncreaseCost(int amt)
+    {
+        if (cost == byte.MaxValue)
+        {
+            return;
+        }
+        if (amt + cost >= 255)
+        {
+            cost = byte.MaxValue;
+        }
+        else
+        {
+            cost += (byte)amt;
+        }
     }
 }
