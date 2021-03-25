4568461ed9904668fbcc8021c9c2026137243968
Dylan Moss <52764361+DylanTMoss@users.noreply.github.com>
2021-03-25 13:08:56 -0500

    scene changes

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -11,7 +11,7 @@ public class GridController : MonoBehaviour
 
     void Start()
     {
-        InvokeRepeating("UpdateGrid", 2.0f, 0.5f);
+        InvokeRepeating("UpdateGrid", 0, 0.5f);
     }
 
     private void InitializeFlowField()
