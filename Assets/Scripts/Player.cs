cdff90d15be1c5ff7e48773bbb1fd074d6818f6e
Tryston Minsquero <tam190005@utdallas.edu>
2021-03-26 02:07:25 -0500

    Created directional movement and sword attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [Header("Attributes")]
+    public int max_health = 3;
     public int moveSpeed;
+    [SerializeField]
+    public float attackRange;
     public Weapon_Type weaponSelected;
 
+    [Header("Globals")]
+    public LayerMask enemyLayer;
+
+    public Transform attackPoint;
+
+    private int health;
+    private Direction facing;
 
     Rigidbody2D rb;
     Controls controls;
@@ -20,8 +32,10 @@ public class Player : MonoBehaviour
     {
         switch (weaponSelected)
         {
+
             case Weapon_Type.SWORD:
                 Debug.Log("SWORD!");
+                SwordAttack();
                 break;
             case Weapon_Type.BLUNT:
                 Debug.Log("BLUNT!");
@@ -42,6 +56,7 @@ public class Player : MonoBehaviour
         switch (weaponSelected)
         {
             case Weapon_Type.SWORD:
+                changeToSword();
                 Debug.Log("changed to SWORD!");
                 break;
             case Weapon_Type.BLUNT:
@@ -51,6 +66,46 @@ public class Player : MonoBehaviour
                 Debug.Log("changed to DISCUS!");
                 break;
         }
+    }
+    private void changeToSword()
+    {
+        weaponSelected = Weapon_Type.SWORD;
+        attackRange = 1;
+        switch (facing)
+        {
+            case Direction.UP:
+                attackPoint.position = transform.position + new Vector3(0,.5f,0);
+                break;
+            case Direction.DOWN:
+                attackPoint.position = transform.position + new Vector3(0, -.5f, 0);
+                break;
+            case Direction.LEFT:
+                attackPoint.position = transform.position + new Vector3(-.5f, 0, 0);
+                break;
+            case Direction.RIGHT:
+                attackPoint.position = transform.position + new Vector3(.5f, 0, 0);
+                break;
+        }
+    }
+
+    private void SwordAttack()
+    {
+        Collider2D[] collidersHit = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in collidersHit)
+        {
+            Debug.Log("Hit Something!");
+        }
+    }
+
+    private void BluntAttack()
+    {
+        Debug.Log("Attack with Blunt");
+    }
+    
+    private void DiscusAttack()
+    {
+        Debug.Log("Attack with Discus");
     }
 
     private void Update()
@@ -66,11 +121,73 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.position = rb.position + moveSpeed * controls.Gameplay.Movement.ReadValue<Vector2>() * Time.fixedDeltaTime;
+        Vector2 direction = controls.Gameplay.Movement.ReadValue<Vector2>();
+        rb.position = rb.position + moveSpeed * direction * Time.fixedDeltaTime;
 
+        Direction dir = facing;
+        //update direction
+        if (direction.y > 0)
+            facing = Direction.UP;
+        if (direction.y < 0)
+            facing = Direction.DOWN;
+        if (direction.x > 0)
+            facing = Direction.RIGHT;
+        if (direction.x < 0)
+            facing = Direction.LEFT;
+
+        if (dir != facing)
+            showDirection();
+            
+        changeDirection();
+    }
+
+
+
+    private void changeDirection()
+    {
+        
+        changeToSword();
+
+        //change animation
+
+    }
+
+    private void showDirection()
+    {
+        switch (facing)
+        {
+            case Direction.UP:
+                Debug.Log("UP");
+                break;
+            case Direction.DOWN:
+                Debug.Log("DOWN");
+                break;
+            case Direction.LEFT:
+                Debug.Log("LEFT");
+                break;
+            case Direction.RIGHT:
+                Debug.Log("RIGHT");
+                break;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
     }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint != null)
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
+
+
+enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 }
