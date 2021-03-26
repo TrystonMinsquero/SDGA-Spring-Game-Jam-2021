d8e25cc5e579ad83af7902721c94264d111715a1
Dylan Moss <52764361+DylanTMoss@users.noreply.github.com>
2021-03-26 11:15:53 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,36 +7,39 @@ public class Enemy : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private bool attackDebounce;
     public Rigidbody2D rb;
-    public float[] weaknesses = { 0, 0, 0 }; // damage multiplier based on attack type
+    public float[] weaknesses = new float[] {0,0,0}; // damage multiplier based on attack type
     public BoxCollider2D hitbox;
     public int type; // 1 = melee 2 = ranged
     public float currentHP;
     public float maximumHP;
     public float forceVal = 2000f;
-
+    private int attackCooldown = 2;
     
     
     // Start is called before the first frame update
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         attackDebounce = false;
     }
 
+
     void OnTriggerEnter2D(Collider2D col)
     {
+        
         if (col.gameObject.name == "Player Temp" && !attackDebounce) {
             attackDebounce = true;
             Vector3 moveDir = transform.position - col.gameObject.transform.position;
             rb.AddForce(moveDir * 2000f);
             Debug.Log("attack");
-            Sleep(2);
+            StartCoroutine(Sleep());
             attackDebounce = false;
         }
     }
 
-    IEnumerator Sleep(float time) {
-        yield return new WaitForSeconds(time);
+    IEnumerator Sleep() {
+        yield return new WaitForSeconds(5);
     }
 
     public void takeDamage(Weapon_Type weapon, int damage)
