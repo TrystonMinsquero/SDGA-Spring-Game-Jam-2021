e9c34b5241e46cb6138608d81e54e81648ff7d61
Dylan Moss <52764361+DylanTMoss@users.noreply.github.com>
2021-03-26 10:35:33 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,11 @@ public class Enemy : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private bool attackDebounce;
     public BoxCollider2D weaponCollider;
-    public int[] weaknesses = { 0, 0, 0 }; // damage multiplier based on attack type
+    public float[] weaknesses = { 0, 0, 0 }; // damage multiplier based on attack type
     public Rigidbody2D target;
     public int type; // 1 = melee 2 = ranged
-    public double currentHP;
-    public double maximumHP;
+    public float currentHP;
+    public float maximumHP;
     public float moveVal = 0.1f;
 
     
@@ -26,15 +26,27 @@ public class Enemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "Player Temp" && !attackDebounce) {
-            transform.position = Vector3.MoveTowards(transform.position, col.gameObject.transform.position, moveVal);
-            attackDebounce = true;
-            Debug.Log("aja");
-            Sleep(2);
-            attackDebounce = false;
+            
         }
     }
 
     IEnumerator Sleep(float time) {
         yield return new WaitForSeconds(time);
+    }
+
+    public void takeDamage(Weapon_Type weapon, int damage)
+    {
+        switch(weapon)
+        {
+            case Weapon_Type.SWORD:
+                currentHP -= damage * weaknesses[0];
+                break;
+            case Weapon_Type.BLUNT:
+                currentHP -= damage * weaknesses[1];
+                break;
+            case Weapon_Type.DISCUS:
+                currentHP -= damage * weaknesses[2];
+                break;
+        }
     }
 }
