a38338a4a56a461d9a1cdbb36b94b30eb04351c3
Tryston Minsquero <tam190005@utdallas.edu>
2021-03-28 00:07:17 -0500

    Level Manager start

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public List<GameObject> spawnPoints;
+    public List<GameObject> enemyTypes;
+
+    public void Spawnenemies(int num)
+    {
+
+    }
+}
